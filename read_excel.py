685788614b4479861a57e5299b8a656cc5e965dc
Lenovo <slayfune@yeah.net>
2020-08-07 15:05:05 +0800

    修改读取文件的问题

--- read_excel.py
+++ read_excel.py
@@ -14,27 +14,26 @@ def get_excel(excel_name):
     # 打开excel并保存内容至open_excel
     open_excel = xlrd3.open_workbook(get_excel_path(excel_name))
     # 打开指定Sheet的内容保存至excel_sheet
-    excel_sheet = open_excel.sheet_by_name("Sheet1")
+    excel_sheet_main = open_excel.sheet_by_name("Sheet1")
     # 获取这个Sheet中内容的行数
-    lines = excel_sheet.nrows
+    lines = excel_sheet_main.nrows
     # 循环取值，排除标题无用数据等
     for line in range(lines):
-        if excel_sheet.row_values(line)[0] != "case_name":
-            cls.append(excel_sheet.row_values(line))
+        if excel_sheet_main.row_values(line)[0] != "case_name":
+            cls.append(excel_sheet_main.row_values(line))
     return cls
 
 # 写入excel文件
-def write_excel():
+def write_excel(i , j):
     myWorkbook = xlwt.Workbook()
     # 数据格式
     myStyle = xlwt.easyxf('font: name Times New Roman, color-index red, bold on', num_format_str='#,##0.00')
+
     mySheet.write(i, j, 1234.56, myStyle)
-    # 写入A3，数值等于1
-    mySheet.write(2, 0, 1)
     #保存
     myWorkbook.save('excelFile.xls')
 
-# 修改excel文件
+
 
 
 if __name__ == '__main__':
